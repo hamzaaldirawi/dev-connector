2f1973851634cb12fe67a9095b35b832f89f7c0b
hamzaaldirawi <hamza.dirawie@gmail.com>
2021-06-17 00:30:08 +0300

    Save

---
+++ client/src/utils/makeAuthHeader.js
@@ -0,0 +1,13 @@
+const makeAuthTokenHeader = token => {
+    if (token) {
+      return {
+        headers: {
+          'x-auth-token': token
+        }
+      }
+    } else {
+      throw new Error('Token is missing.');
+    }
+  }
+   
+  export default makeAuthTokenHeader;
