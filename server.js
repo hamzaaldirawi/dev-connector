49e4bc46fcbb8c554a0e41a6ad68909957f66e9b
hamzaaldirawi <hamza.dirawie@gmail.com>
2021-06-25 01:33:15 +0300

    Final

--- server.js
+++ server.js
@@ -3,20 +3,34 @@ const connectDB = require('./db/db')
 const usersRouter = require('./routes/api/users')
 const authRouter = require('./routes/api/auth')
 const app = express()
-
+const path = require('path')
 // Connect DB
 connectDB()
 
 // init Middleware
 app.use(express.json({extended: false}))
- 
-app.get('/', (req, res) => res.send('API Running'))
+
+// For Development only
+if(process.env.NODE_ENV === 'development') {
+    app.get('/', (req, res) => res.send('API Running'))
+}
+
 
 // Define Routes // in 2 different way
 app.use(usersRouter)
 app.use(authRouter)
 app.use('/api/profile', require('./routes/api/profile'))
 app.use('/api/posts', require('./routes/api/posts'))
+
+// Serve static assets in production
+if(process.env.NODE_ENV === 'production') {
+    //set static folder
+    app.use(express.static('client/build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 const PORT = process.env.PORT
 
